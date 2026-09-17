41de99fb4bffa667d3d86e0dbdba0ee5aea5b02a
mguib <marcos.silva@softlive.dev>
2026-09-17 14:46:15 -0400

    feat: Adding icons for project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.mguibtech.triprateapp"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.mguibtech.triprateapp"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/mguibtech/triprateapp/ui/feature/converter/ConverterScreen.kt
@@ -0,0 +1,25 @@
+package com.mguibtech.triprateapp.ui.feature.converter
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mguibtech.triprateapp.ui.theme.TripRateAppTheme
+
+@Composable
+fun ConverterScreen() {
+    
+}
+
+
+@Composable
+fun ConverterContent() {
+    
+}
+
+@Composable
+@Preview
+fun ConverterContentPreview() {
+    TripRateAppTheme {
+        ConverterContent()
+    }
+}
